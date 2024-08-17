a4ac095253b9cb865b46781b295bf1ed74d7c6b8
Ollie Chapman <57490535+OllieeeChap@users.noreply.github.com>
2024-08-17 20:33:12 +0200

    update level

---
+++ scripts/cat_display_sprite.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Set viewporte texture path
+	var viewport_texture = get_parent().get_node("SubViewport").get_texture()
+	self.texture = viewport_texture
+	
