d7248bbbe377186ffc7d8ff4669b9667c1ee4256
Ollie Chapman <57490535+OllieeeChap@users.noreply.github.com>
2024-08-19 13:35:13 +0200

    Add aligned detection

--- scripts/webcamControls.gd
+++ scripts/webcamControls.gd
@@ -7,7 +7,23 @@ var _zoom_speed: float = 1
 @onready var camera = $world/WebcamPivot/Camera3D;
 @onready var camera_pivot = $world/WebcamPivot
 
+# Keep camera default FOV and position
+var cam_default_pos_x
+var cam_default_pos_y
+var cam_default_fov
+
+# Margin for ears detection
+@export var EARS_MARGIN_POS = 2
+@export var EARS_MARGIN_FOV = 5
+
 var is_aligned = false
+
+func _on_ready() -> void:
+	# Keep default
+	cam_default_pos_x = camera_pivot.rotation_degrees.x
+	cam_default_pos_y = camera_pivot.rotation_degrees.y
+	cam_default_fov = camera.fov	
+	# Randomize FOV and position
 
 func _process(delta: float) -> void:
 	if !is_active:
@@ -28,6 +44,22 @@ func _process(delta: float) -> void:
 			rotate_webcam_y(_pan_speed)
 		elif Input.is_action_pressed("ui_right"):
 			rotate_webcam_y(-_pan_speed)
+			
+	# Check if ears are aligned
+	if (
+		# X axis
+		camera_pivot.rotation_degrees.x < (cam_default_pos_x + EARS_MARGIN_POS) &&
+		camera_pivot.rotation_degrees.x > (cam_default_pos_x - EARS_MARGIN_POS) &&
+		# Y axis
+		camera_pivot.rotation_degrees.y < (cam_default_pos_y + EARS_MARGIN_POS) &&
+		camera_pivot.rotation_degrees.y > (cam_default_pos_y - EARS_MARGIN_POS) &&
+		# FOV
+		camera.fov < (cam_default_fov + EARS_MARGIN_FOV) &&
+		camera.fov > (cam_default_fov - EARS_MARGIN_FOV)
+	) :
+		is_aligned = true
+	else :
+		is_aligned = false
 
 func zoom(speed) -> void:
 	camera.fov += speed
