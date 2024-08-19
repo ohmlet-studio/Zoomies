36bbc363f9475a788c1225a507bd042cdc8ca34d
Ollie Chapman <57490535+OllieeeChap@users.noreply.github.com>
2024-08-20 01:30:55 +0200

    Add audio buses and modified main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,8 @@
 extends Control
 
 @onready var buttons_v_box = $MarginContainer/ButtonsVBox
+@onready var start_container = %StartContainer
+@onready var option_container = %OptionContainer
 
 func _ready() -> void:
 	focus_button()
@@ -25,4 +27,16 @@ func _on_visibility_changed():
 
 
 func _on_options_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/ui/options_menu.tscn")
+	start_container.visible = false
+	option_container.visible = true
+
+# ------------------ Option menu ----------------------
+func _on_return_button_pressed() -> void:
+	start_container.visible = true
+	option_container.visible = false
+	
+
+
+
+func _on_music_ctrl_slider_drag_ended(value_changed: bool) -> void:
+	pass # Replace with function body.
