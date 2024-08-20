ee1a1f39e8ddffa4dab0c8fe66a665de1690aa7c
Ollie Chapman <57490535+OllieeeChap@users.noreply.github.com>
2024-08-20 16:39:16 +0200

    Add gameover when timer ends

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,7 +8,8 @@ func _ready() -> void:
 	focus_button()
 	
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/intro/part_one.tscn")
+	#get_tree().change_scene_to_file("res://scenes/intro/part_one.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
 	
 func _on_infinite_mode_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/level_infinite.tscn")
