c74f2772d4df8f6504aef55b8504665172757863
Ollie Chapman <57490535+OllieeeChap@users.noreply.github.com>
2024-08-20 02:37:25 +0200

    Update pause menu

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -5,3 +5,24 @@ extends Control
 func _on_return_button_pressed() -> void:
 	hide()
 	get_tree().paused = false
+
+
+
+func _on_music_ctrl_slider_value_changed(value: float) -> void:
+	var val_temp
+	if value <= -50 :
+		val_temp = -80
+	else:
+		val_temp = value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), val_temp)
+	
+
+
+
+func _on_sfx_ctrl_slider_value_changed(value: float) -> void:
+	var val_temp
+	if value <= -50 :
+		val_temp = -80
+	else:
+		val_temp = value
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), val_temp)
